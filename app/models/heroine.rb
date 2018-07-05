70868a20f100a59ff33e2a7e02451062a420455c
github username <github email address>
2018-07-05 11:15:43 +0100

    finished

--- app/models/heroine.rb
+++ app/models/heroine.rb
@@ -1,2 +1,4 @@
 class Heroine < ApplicationRecord
+    validates  :super_name, presence: true, uniqueness:true
+    belongs_to :power
 end
--- app/models/power.rb
+++ app/models/power.rb
@@ -1,2 +1,6 @@
 class Power < ApplicationRecord
+    validates :name, presence: true
+    validates :description, presence: true
+
+    has_many :heroines
 end
